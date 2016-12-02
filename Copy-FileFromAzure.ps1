11866c7d92a1ed8efc62e764da722fbf20070487
Dwai Banerjee <dwaiba@microsoft.com>
2016-12-02 12:18:26 +0100

    Update Copy-FileFromAzure.ps1

--- Copy-FileFromAzure.ps1
+++ Copy-FileFromAzure.ps1
@@ -1,12 +1,11 @@
 <# Custom Script for Windows to install a file from Azure Storage using the staging folder created by the deployment script #>
 param (
     [string]$artifactsLocation,
-    [string]$artifactsLocationSasToken,
     [string]$folderName,
     [string]$fileToInstall
 )
 
-$source = $artifactsLocation + "\$folderName\$fileToInstall" + $artifactsLocationSasToken
+$source = $artifactsLocation + "\$folderName\$fileToInstall"
 $dest = "C:\WindowsAzure\$folderName"
 New-Item -Path $dest -ItemType directory
 Invoke-WebRequest $source -OutFile "$dest\$fileToInstall"
