e8f3cdaea090dedbc1b1ddd9840d3eb3f110a949
Dwai Banerjee <dwaiba@microsoft.com>
2016-12-06 22:46:42 +0100

    Update and rename Copy-FileFromAzure.ps1 to installDriverAgentsM60.ps1

---
+++ installDriverAgentsM60.ps1
@@ -0,0 +1,42 @@
+<# Custom Script for Windows to install a file from Azure Storage using the staging folder created by the deployment script #>
+param (
+    [string]$dest,
+    [string]$leostreamAgentVer,
+    [string]$teradiciAgentVer,
+    [string]$nvidiaVer,	
+	[string]$storageAcc,
+	[string]$conName
+)
+$dest = "C:\Downloadinstallers"
+
+$leostreamAgentVer = "6-2-7-0"
+
+$teradiciAgentVer = "2.7.0.3589"
+
+$nvidiaVer = "369.71"
+
+$storageAcc = "tdcm16sg112leo8193ls102"
+
+$conName = "tdcm16sg112leo8193ls102"
+
+
+
+New-Item -Path $dest -ItemType directory
+
+wget https://$storageAcc.blob.core.windows.net/$conName/"$nvidiaVer"_grid_win10_server2016_64bit_international.exe -OutFile C:\Downloadinstallers\"$nvidiaVer"_grid_win10_server2016_64bit_international.exe
+wget http://download.opendtect.org/relman/OpendTect_Installer_win64.exe -OutFile C:\Downloadinstallers\OpendTect_Installer_win64.exe
+wget https://$storageAcc.blob.core.windows.net/$conName/PCoIP_agent_release_installer_"$teradiciAgentVer"_graphics.exe -OutFile C:\Downloadinstallers\PCoIP_agent_release_installer_"$teradiciAgentVer"_graphics.exe
+
+wget https://$storageAcc.blob.core.windows.net/$conName/LeostreamAgentSetup$leostreamAgentVer.exe -OutFile C:\Downloadinstallers\LeostreamAgentSetup$leostreamAgentVer.exe
+
+
+C:\Downloadinstallers\*_grid_win10_server2016_64bit_international.exe /s
+Set-Location "C:\NVIDIA\*"
+.\setup.exe -s
+Start-Sleep -s 90
+C:\Downloadinstallers\PCoIP_agent_release_installer_* /S
+Start-Sleep -s 90
+& 'C:\Program Files (x86)\Teradici\PCoIP Agent\bin\RestartAgent.bat'
+net stop nvsvc
+Start-Sleep -s 90
+net start nvsvc
