2c4d2dc7a7f316af6d500b32d432adf7d0d3ccc0
Mike Kiernan <mkiernan@microsoft.com>
2017-02-12 09:42:40 +0400

    update Adminpassword

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@
 #
 az group create --location "West Europe"  --name "vizgrp"
 # deploy from local template files
-#az group deployment create -g vizgrp --template-file azuredeploy.json --parameters @azuredeploy.parameters.json
+az group deployment create -g vizgrp --template-file azuredeploy.json --parameters @azuredeploy.parameters.json
 # deploy from github template [you can also add --parameters followed by a .json string as well as the parameters file method above]
-az group deployment create -g vizgrp --template-uri https://raw.githubusercontent.com/mkiernan/azure-teradici-demo-windows-gpu/master/azuredeploy.json 
+#az group deployment create -g vizgrp --template-uri https://raw.githubusercontent.com/mkiernan/azure-teradici-demo-windows-gpu/master/azuredeploy.json
